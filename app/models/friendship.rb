78c738cd3de09230499b2d84ce3234da9ee107ad
kohei12 <kohei122001@yahoo.co.jp>
2016-11-10 22:09:41 +0900

    Fix friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,41 +1,35 @@
 class Friendship < ActiveRecord::Base
-  belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
+  # リクエストを送ったユーザー
+  belongs_to :requested_user, class_name: 'User', foreign_key: 'user_id'
   has_one :room, dependent: :destroy
 
   validates :user_id,
+    presence: true,
     uniqueness: { scope: :friend_id }
+  validates_presence_of :friend_id
   validates_presence_of :status
 
-  enum status: { 'pending': 1, 'requested': 2, 'accepted': 3 }.freeze
+  enum status: { pending: 1, accepted: 2, rejected: 3 }.freeze
 
   def self.request(user, friend)
-    unless user == friend || inverse_request(user, friend).present? || already_requested?(user, friend)
+    unless user == friend || already_requested?(user, friend)
       transaction do
-        create(user: user, friend: friend, status: 'pending')
-        create(user: friend, friend: user, status: 'requested')
+        create(requested_user: user, friend: friend, status: 'pending')
       end
     end
   end
 
   def accept(user, friend)
-    transaction do
-      accepted_at = Time.now
-      accept_one_side(user, friend, accepted_at)
-      accept_one_side(friend, user, accepted_at)
-      room = Room.create(user, friend)
+    unless user == friend || self.requested_user == user
+      transaction do
+        self.update!(
+         accepted_at: Time.now,
+         status: 'accepted',
+         room: Room.create(user, friend, self)
+        )
+      end
     end
-  end
-
-  def accept_one_side(user, friend, accepted_at)
-    request = Friendship.find_by_user_id_and_friend_id(user ,friend)
-    request.status = 'accepted'
-    request.accepted_at = accepted_at
-    request.save!
-  end
-
-  def self.inverse_request(user, friend)
-    find_by_user_id_and_friend_id(friend, user)
   end
 
   def self.already_requested?(user, friend)
