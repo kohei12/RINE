f1f487ee2e784f23ae7192a20deb1d222e40b76e
kohei12 <kohei122001@yahoo.co.jp>
2016-11-11 12:47:36 +0900

    Fix destroy action of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     if @user.destroy
+      session[:user_id] = nil
       redirect_to sign_up_path
     else
       redirect_to @user
