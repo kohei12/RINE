8b5943da2a99f8323da27db78210bcaea5a93b1a
kohei12 <kohei122001@yahoo.co.jp>
2016-11-11 12:18:01 +0900

    Fix user model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,10 @@
 class Message < ActiveRecord::Base
-  after_create_commit { MessageBroadcastJob.perform_later self }
+  after_create_commit { BroadcastMessageJob.perform_later self }
 
-  #belongs_to :room
-  #belongs_to :sent_user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :sent_user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :room
 
-  validates_presence_of :text
-  #validates_presence_of :sent_user
-  #validates_presence_of :room
+  validates_presence_of :content
+  validates_presence_of :sent_user
+  validates_presence_of :room
 end
