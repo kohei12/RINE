40964ae6bfe073eeafb0d9e24aab3d137c36993d
kohei12 <kohei122001@yahoo.co.jp>
2016-11-19 00:32:47 +0900

    Fix broad cast job

--- app/jobs/broadcast_message_job.rb
+++ app/jobs/broadcast_message_job.rb
@@ -2,12 +2,12 @@ class BroadcastMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast 'room_channel', message: render_message(messge)
+    ActionCable.server.broadcast "room_channel_#{message.room_id}", message: render_message(message)
   end
 
   private
 
   def render_message(message)
-    ApplicationController.renderer.render message
+    ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
   end
 end
